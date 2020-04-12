6fc1aa0aca1bb22b40bd134506e2cabfa0d7ad1b
Thales Vinícius <thalesburakowski@gmail.com>
2020-04-11 22:18:51 -0300

    feat: creating core of project

---
+++ src/app.js
@@ -0,0 +1,10 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+})
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+module.exports = app
