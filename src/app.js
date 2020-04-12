e02b5556420ac5cbcc2e33efdc0bcdec9110126d
Thales Vinícius <thalesburakowski@gmail.com>
2020-04-12 04:19:44 -0300

    feat: initiate database when app starts

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ require('dotenv').config({
   path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 })
 
+require('./database')
+
 const express = require('express')
 const app = express()
 
--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,28 @@
 const Sequelize = require('sequelize')
+const fs = require('fs')
+const path = require('path')
 const dbConfig = require('../config/database')
 const connection = new Sequelize(dbConfig)
 
+initAllModels()
+
 module.exports = connection
+
+function initAllModels () {
+  const normalizedPath = require('path').resolve('src', 'app', 'models')
+  const folders = fs.readdirSync(normalizedPath)
+  folders.forEach((folder) => {
+    const actualPath = path.join(normalizedPath, folder)
+    const modelPath = fs
+      .readdirSync(actualPath)
+      .filter((file) => file.includes('model'))
+    modelPath.forEach((modelFile) => {
+      const model = require(path.join(actualPath, modelFile))
+      model.init(connection)
+      if (model.associate) {
+        model.associate(connection)
+      }
+      model.sync()
+    })
+  })
+}
